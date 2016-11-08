d5ef5819b8041b20bbc0b3872a4779bd94e5febf
cafemocha0804 <moka_tabata@tabatamoenatsu-no-MacBook-Air.local>
2016-11-08 19:18:37 +0900

    復習

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,57 @@
+
+//練習問題２　二つの数の割り算をする関数
 function div(wararerukazu,warukazu){
  let result;
  result= wararerukazu / warukazu
  return result;
 }
 
+
+//練習問題３　ある数を 1.6 で割る関数
 function toMile(km){
 let result;
 result= div(km,1.6);
 return result;
 }
 
+
+//練習問題４　toMile の逆の計算を行う関数
 function toKM(mile){
 let result;
 result= mile * 1.6;
 return result;
 }
 
+
+/*練習問題１　論理和の否定
+let a, b, c, d;
+a = true || true; // true
+b = true || false; // true
+c = false || true; // true
+d = false || false; // false */
 function nand(a, b){
   let c = a && b;
   return !c; 
 }
 
+/*練習問題２　論理積の否定
+let a, b, c, d;
+a = true || true; // true
+b = true || false; // false
+c = false || true; // false
+d = false || false; // false */
 function nor(a, b){
   let c = a || b;
   return !c; 
 }
 
+/*練習問題３
+let a,b,c,d
+a = true || true;	// false
+b = true || false; // true
+c = false || true; // true
+d = false || false; // false */
+function xor(a,b){
+  let c = a == b;
+  return !c;
+}
--- scripts/test.js
+++ scripts/test.js
@@ -1,12 +1,20 @@
+//練習問題５ 引数に指定された 2つの値が等しいことを調べる関数
 function assert_eq(a, b){
     return a == b;
 }
 
+//練習問題６ xor の正常動作を確認する関数
+function test_xor(){
+
+}
+
+//練習問題７ norの正常動作を確認する関数
 function test_nor(){
     return nor(true, true) == false &&
      nor(true, false) == false;
 }
 
+//練習問題７ norの正常動作を確認する関数
 function test_nor(){
     return assert_eq(nor(true, true) , false) &&
      assert_eq(nor(true, false) , false) &&
